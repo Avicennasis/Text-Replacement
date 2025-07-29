2b511b53fb3552660bcaf786fdfc5a112e87f089
Léon Simmons <Avicennasis@gmail.com>
2025-07-29 15:21:44 -0400

    Update content.js
    Added regex escaping to make the extension more robust and user-friendly. Added Conditional Word Boundaries based on text being replaced.

--- content.js
+++ content.js
@@ -1,11 +1,25 @@
+// content.js
+
+function escapeRegExp(string) {
+  // Escapes special characters for use in a regular expression.
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
 function replaceTextInNode(node, wordMap) {
   // Only process text nodes
   if (node.nodeType === 3) {
     let text = node.nodeValue;
     for (const [word, { replacement, caseSensitive }] of Object.entries(wordMap)) {
       if (replacement === undefined) continue;
+
+      const escapedWord = escapeRegExp(word);
       const regexFlags = caseSensitive ? 'g' : 'gi';
-      const regex = new RegExp(`\\b${word}\\b`, regexFlags);
+
+      // Conditionally add word boundaries
+      const prefix = /^\w/.test(word) ? '\\b' : '';
+      const suffix = /\w$/.test(word) ? '\\b' : '';
+      
+      const regex = new RegExp(`${prefix}${escapedWord}${suffix}`, regexFlags);
       text = text.replace(regex, replacement);
     }
     node.nodeValue = text;
