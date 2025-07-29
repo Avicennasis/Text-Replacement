7fdc993dd3531d15348a91797b49310559558459
Léon Simmons <Avicennasis@gmail.com>
2025-07-29 14:59:40 -0400

    Update background.js
    Removed the message passing logic and have content.js call chrome.storage.sync.get() itself. The onMessage listener is no longer needed and can be removed, simplifying the background script significantly.

--- background.js
+++ background.js
@@ -1,13 +1,3 @@
 chrome.runtime.onInstalled.addListener(() => {
-  console.log('Extension installed!');
-});
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === 'getWordMap') {
-    chrome.storage.sync.get('wordMap', (data) => {
-      sendResponse(data);
-    });
-    // Indicate that the response will be sent asynchronously
-    return true;
-  }
+  console.log('Avic\'s Text Replacement extension installed!');
 });
